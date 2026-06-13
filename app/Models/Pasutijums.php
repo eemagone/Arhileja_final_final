0f5fcac4ce96a74718ee03bfb6dafa5c28caf025
Emils <emilsmagone1@gmail.com>
2026-06-13 19:21:44 +0300

    13/06/2026 19:21

--- app/Models/Pasutijums.php
+++ app/Models/Pasutijums.php
@@ -7,28 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Pasutijums extends Model
 {
     protected $table = 'Pasutijumi';
-    protected $primaryKey = 'Pasutijuma_ID';
+    
+    // LABOTS: Bija 'Pasutijuma_ID', bet datubāzē ir 'Pasutijumi_ID'
+    protected $primaryKey = 'Pasutijumi_ID'; 
     public $timestamps = false;
 
+    // LABOTS: Izņemti Klienta_ID un Filiales_ID, jo to nav Pasutijumi tabulā
     protected $fillable = [
-        'Klienta_ID', 'Filiales_ID', 'Apavu_ID', 'Meistara_ID', 
-        'Pienemsanas_Datums', 'Izpildes_Termins', 'Apraksts', 
+        'Apavu_ID', 'Meistara_ID', 
+        'Pienemsanas_Datums', 'Termins', 'RemontaVeids', 
         'Statuss', 'Cena', 'Garantijas_Termins'
     ];
 
-    public function klients()
-    {
-        return $this->belongsTo(Klients::class, 'Klienta_ID', 'Klienta_ID');
-    }
-
-    public function filiale()
-    {
-        return $this->belongsTo(Filiale::class, 'Filiales_ID', 'Filiales_ID');
-    }
-
     public function apavi()
     {
-        return $this->belongsTo(Apavi::class, 'Apavu_ID', 'Apavu_ID');
+        // LABOTS: Trešais parametrs tagad ir 'Apavi_ID'
+        return $this->belongsTo(Apavi::class, 'Apavu_ID', 'Apavi_ID');
     }
 
     public function meistars()
@@ -38,12 +32,14 @@ class Pasutijums extends Model
 
     public function atsauksme()
     {
-        return $this->hasOne(Atsauksme::class, 'Pasutijuma_ID', 'Pasutijuma_ID');
+        // Šeit atstājam Pasutijuma_ID, jo Atsauksmes tabulā ārējā atslēga ir Pasutijuma_ID
+        return $this->hasOne(Atsauksme::class, 'Pasutijuma_ID', 'Pasutijumi_ID');
     }
 
     public function materiali()
     {
+        // Šeit arī atstājam Pasutijuma_ID kā Pivot kolonnu
         return $this->belongsToMany(Materiali::class, 'PasutijumaMateriali', 'Pasutijuma_ID', 'Materiali_ID')
-                    ->withPivot('Daudzums'); // Lai piekļūtu patērētajam skaitam
+                    ->withPivot('Daudzums');
     }
 }
